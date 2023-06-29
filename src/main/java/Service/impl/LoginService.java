2c676f02613f62e18ffac3f8ff869c5194954d76
Léo Lhomme <leolhommeo@gmail.com>
2023-06-29 11:12:04 +0200

    reecriture test LoginService

--- src/main/java/Service/impl/LoginService.java
+++ src/main/java/Service/impl/LoginService.java
@@ -2,9 +2,19 @@ package Service.impl;
 
 import Entity.User;
 import Service.ILoginService;
+import jakarta.servlet.http.HttpSession;
 import jdk.jshell.spi.ExecutionControl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoginService implements ILoginService {
+
+    private HttpSession httpSession;
+
+    public void setHttpSession(HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
+
     @Override
     public boolean login(User user) throws ExecutionControl.NotImplementedException {
         throw new ExecutionControl.NotImplementedException("not implemented");
--- src/test/java/Service/impl/LoginServiceTest.java
+++ src/test/java/Service/impl/LoginServiceTest.java
@@ -2,16 +2,22 @@ package Service.impl;
 
 import Entity.User;
 import Repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import jdk.jshell.spi.ExecutionControl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 
 public class LoginServiceTest {
     private LoginService loginService;
+
+    @Mock
+    private HttpSession httpSession;
 
     @BeforeEach
     void setUp (){
@@ -20,50 +26,57 @@ public class LoginServiceTest {
 
     @Test
     void testIsLoggedWithSessionIsLoggedFalse () throws ExecutionControl.NotImplementedException {
+        loginService.setHttpSession(httpSession);
+        Mockito.when(httpSession.getAttribute("")).thenReturn(null);
         Assertions.assertFalse(loginService.isLogged());
     }
     @Test
     void testIsLoggedWithSessionIsLoggedtrue () throws ExecutionControl.NotImplementedException {
-        User user = new User(1,"toto","tata","toto@tata.com","password",true,new ArrayList<>());
-        loginService.login(user);
+
+        Mockito.when(httpSession.getAttribute("isLogged")).thenReturn(true);
+        loginService.setHttpSession(httpSession);
         Assertions.assertTrue(loginService.isLogged());
     }
 
     @Test
     void testIsAdminWithSessionIsAdminFalse () throws ExecutionControl.NotImplementedException {
-        User user = new User(1,"toto","tata","toto@tata.com","password",false,new ArrayList<>());
-        loginService.login(user);
+        Mockito.when(httpSession.getAttribute("isAdmin")).thenReturn(false);
+        loginService.setHttpSession(httpSession);
         Assertions.assertFalse(loginService.isAdmin());
     }
 
     @Test
     void testIsAdminWithSessionIsAdmintrue () throws ExecutionControl.NotImplementedException {
-        User user = new User(1,"toto","tata","toto@tata.com","password",true,new ArrayList<>());
-        loginService.login(user);
+        Mockito.when(httpSession.getAttribute("isLogged")).thenReturn(true);
+        loginService.setHttpSession(httpSession);
         Assertions.assertFalse(loginService.isAdmin());
     }
 
     @Test
     void testGetUserIdWithSessionUserIdEqualsNull () throws ExecutionControl.NotImplementedException {
+        Mockito.when(httpSession.getAttribute("userId")).thenReturn(null);
+        loginService.setHttpSession(httpSession);
         Assertions.assertNotEquals(1,loginService.getUserId());
     }
 
     @Test
     void testGetUserIdWithSessionUserIdEqual0 () throws ExecutionControl.NotImplementedException {
-        User user = new User(1,"toto","tata","toto@tata.com","password",true,new ArrayList<>());
-        loginService.login(user);
+        Mockito.when(httpSession.getAttribute("userId")).thenReturn(1);
+        loginService.setHttpSession(httpSession);
         Assertions.assertEquals(1,loginService.getUserId());
     }
 
     @Test
     void testGetUserFullNameWithSessionUserFullNameEqualNull () throws ExecutionControl.NotImplementedException {
+        Mockito.when(httpSession.getAttribute("fullname")).thenReturn(null);
+        loginService.setHttpSession(httpSession);
         Assertions.assertNotEquals("toto tata",loginService.getUserFullname());
     }
 
     @Test
     void testGetUserFullNameWithSessionUserFullNameEqualToto_Tata () throws ExecutionControl.NotImplementedException {
-        User user = new User(1,"toto","tata","toto@tata.com","password",true,new ArrayList<>());
-        loginService.login(user);
+        Mockito.when(httpSession.getAttribute("fullname")).thenReturn("toto tata");
+        loginService.setHttpSession(httpSession);
         Assertions.assertEquals("toto tata",loginService.getUserFullname());
     }
 }
